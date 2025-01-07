a472ddb5ea68adb77e04abe158f441ec2bfe808b
carvalhio <raffael.carvallho@gmail.com>
2025-01-07 11:56:14 -0300

    Table Teachers populated. Table teachers styled

--- db/migrate/20250106194534_create_teachers.rb
+++ db/migrate/20250106194534_create_teachers.rb
@@ -2,10 +2,8 @@ class CreateTeachers < ActiveRecord::Migration[7.2]
   def change
     create_table :teachers do |t|
       t.string :first_name
-      t.string :last_name
       t.string :email
       t.string :phone
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_194534) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_07_105839) do
   create_table "teachers", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.string "phone"
     t.datetime "created_at", null: false
