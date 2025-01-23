f777d190f2b74841a77dfdd9e79c92348704de2c
carvalhio <raffael.carvallho@gmail.com>
2025-01-23 17:27:17 -0300

    print only page update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def ensinomedio
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get 'home/about'
+  get 'schedules/ensinomedio'
   root 'home#index'
 
 end
