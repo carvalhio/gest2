d518f64ed2fe077529ae45acd25ac476280953fd
carvalhio <raffael.carvallho@gmail.com>
2025-03-24 16:55:11 -0300

    school_class form updated, includes mode classes

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,8 +1,9 @@
 class SchoolClass < ApplicationRecord
   GRADES = [
-    "Infantil","1º ano", "2º ano", "3º ano", "4º ano", "5º ano",
-    "6º ano", "7º ano", "8º ano", "9º ano",
-    "1ª série", "2ª série", "3ª série"
+      "Infantil I", "Infantil II", "Infantil III", "Infantil IV", "Infantil V",
+      "1º ano", "2º ano", "3º ano", "4º ano", "5º ano",
+      "6º ano", "7º ano", "8º ano", "9º ano",
+      "1ª série", "2ª série", "3ª série"
   ]
 
   IDENTIFIERS = ("A".."Z").to_a + ["II", "III", "IV", "V"]
